9d3fb4434a4f87ad200b3654f04f63d7908fc445
beike-zh <aiyoung0924@gmail.com>
2021-06-01 09:22:02 +0800

    add

--- init/zsh/init.zsh
+++ init/zsh/init.zsh
@@ -44,7 +44,7 @@ alias fdsize='du -h --max-depth=1'
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.dotnet/tools:$PATH"
 export PATH="$HOME/.idris2/bin:$PATH"
-
+export PATH="$HOME/.elan/bin:$PATH"
 
 # Agda
 export Agda_datadir="$HOME/app/Agda-nightly/data"
